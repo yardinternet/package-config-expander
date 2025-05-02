0e8e15d3168ff57b558a57cea517f158bc74201c
Simon <simon@yard.nl>
2025-05-02 16:48:26 +0200

    (fix): emoji scripts and styles not removed in admin

--- src/Disable/DisableEmojicons.php
+++ src/Disable/DisableEmojicons.php
@@ -8,9 +8,13 @@ class DisableEmojicons
 {
 	public static function init(): void
 	{
-		remove_action('admin_print_styles', 'print_emoji_styles');
+		add_action('admin_init', function () {
+			remove_action('admin_print_scripts', 'print_emoji_detection_script');
+			remove_action( 'admin_enqueue_scripts', 'wp_enqueue_emoji_styles' );
+			remove_action('admin_print_styles', 'print_emoji_styles');
+		});
+
 		remove_action('wp_head', 'print_emoji_detection_script', 7);
-		remove_action('admin_print_scripts', 'print_emoji_detection_script');
 		remove_action('wp_print_styles', 'print_emoji_styles');
 		remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
 		remove_filter('the_content_feed', 'wp_staticize_emoji');
