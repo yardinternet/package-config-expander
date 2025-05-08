001bb3cf9f90f1578e47af3a9c561e7d7ba94dd0
ictbeheer <14947039+ictbeheer@users.noreply.github.com>
2025-05-08 13:46:00 +0000

    (auto): apply php-cs-fixer changes

--- src/Disable/DisableEmojicons.php
+++ src/Disable/DisableEmojicons.php
@@ -10,7 +10,7 @@ class DisableEmojicons
 	{
 		add_action('admin_init', function () {
 			remove_action('admin_print_scripts', 'print_emoji_detection_script');
-			remove_action( 'admin_enqueue_scripts', 'wp_enqueue_emoji_styles' );
+			remove_action('admin_enqueue_scripts', 'wp_enqueue_emoji_styles');
 			remove_action('admin_print_styles', 'print_emoji_styles');
 		});
 
