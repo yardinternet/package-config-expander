0808487cde689c0197993d69d6066def854792d6
Mike van den Hoek <mike@yard.nl>
2025-09-19 10:20:09 +0200

    (refactor): protection status

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/yard/config-expander/resources/css/protection.css', function () {
+	return response()->file(__DIR__.'/../../resources/css/protection.css', [
+		'Content-Type' => 'text/css',
+	]);
+});
+
 Route::get('/yard/config-expander/resources/css/login-style.css', function () {
 	return response()->file(__DIR__.'/../../resources/css/login-style.css', [
 		'Content-Type' => 'text/css',
