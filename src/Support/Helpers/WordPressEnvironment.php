bc376bc9707e087b39b9d8596c787b91fbb462ca
mvdhoek1 <11852816+mvdhoek1@users.noreply.github.com>
2025-09-19 04:54:06 +0000

    (auto): apply php-cs-fixer changes

--- src/Support/Helpers/WordPressEnvironment.php
+++ src/Support/Helpers/WordPressEnvironment.php
@@ -6,23 +6,23 @@ namespace Yard\ConfigExpander\Support\Helpers;
 
 final class WordPressEnvironment
 {
-    public static function isDevelopment(): bool
-    {
-        return self::isType('development');
-    }
+	public static function isDevelopment(): bool
+	{
+		return self::isType('development');
+	}
 
-    public static function isStaging(): bool
-    {
-        return self::isType('staging');
-    }
+	public static function isStaging(): bool
+	{
+		return self::isType('staging');
+	}
 
-    public static function isProduction(): bool
-    {
-        return self::isType('production');
-    }
+	public static function isProduction(): bool
+	{
+		return self::isType('production');
+	}
 
-    private static function isType(string $type): bool
-    {
-        return defined('WP_ENV') && WP_ENV === $type;
-    }
+	private static function isType(string $type): bool
+	{
+		return defined('WP_ENV') && WP_ENV === $type;
+	}
 }
